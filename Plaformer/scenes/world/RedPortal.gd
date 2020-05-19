8997f6a5c21abd508f0225e7152536d891f71d93
Lordgard78230 <65163526+Lordgard78230@users.noreply.github.com>
2020-05-19 21:43:01 +0200

    Save and load begin

--- Plaformer/scenes/world/RedPortal.gd
+++ Plaformer/scenes/world/RedPortal.gd
@@ -2,8 +2,24 @@ extends Area2D
 
 export(String, FILE, "*.tscn") var world_scene
 
+
+func _on_RedPortal_body_entered(body: Node) -> void:
+	if body.name == "Player":
+		$Label.show()
+
+
+
+func _on_RedPortal_body_exited(body: Node) -> void:
+	$Label.hide()
+
 func _physics_process(_delta: float) -> void:
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Player":
-			get_tree().change_scene(world_scene)
+			if Input.is_action_just_pressed("F"):
+				get_tree().change_scene(world_scene)
+
+
+
+
+
