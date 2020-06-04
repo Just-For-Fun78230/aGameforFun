aad43c4726adc1f7cc77096ac4a2ed6a8eb1df5f
Lordgard78230 <65163526+Lordgard78230@users.noreply.github.com>
2020-06-04 19:59:46 +0200

    mini shooter update

--- Plaformer/scenes/characters/ennemys/Not moving/MiniShooter/MiniShooter.gd
+++ Plaformer/scenes/characters/ennemys/Not moving/MiniShooter/MiniShooter.gd
@@ -9,13 +9,16 @@ onready var mini_ball_timer = $MiniBallTimer
 var can_shoot = true
 var should_shoot = false
 
-export var ball_speed_x = 0
-export var ball_speed_y = 0
+export var ball_speed_x := 0
+export var ball_speed_y := 0
 
+export var ball_scale_x := .5
+export var ball_scale_y := .5
 
+export var ball_time := 1.0
 
 func _ready() -> void:
-	mini_ball_timer.set_wait_time(1)
+	mini_ball_timer.set_wait_time(ball_time)
 	
 	
 func _on_MiniDaggerTimer_timeout() -> void:
@@ -40,6 +43,8 @@ func _physics_process(delta: float) -> void:
 func mini_dagger_fire(delta):
 	var mini_ball = BALL.instance()
 	get_parent().add_child(mini_ball)
+	mini_ball.scale.x = ball_scale_x
+	mini_ball.scale.y = ball_scale_y
 	mini_ball.ball_velocity.x = ball_speed_x * delta
 	mini_ball.ball_velocity.y = ball_speed_y * delta
 	mini_ball.position = $Body/MiniBallSpawner.global_position
